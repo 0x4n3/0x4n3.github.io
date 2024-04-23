27bcc3e62e3b9e1814b22afdded0145a4e50f857
Primary <nathaniel@augeneric.com>
2024-04-23 02:38:53 -0400

    Update

---
+++ geoffchappell/shell/download_shell.ps1
@@ -0,0 +1,19 @@
+$outputdir = 'D:\Geoff Chappell\shell'
+$url       = 'https://www.geoffchappell.com/studies/windows/shell/toc.htm'
+
+# enable TLS 1.2 and TLS 1.1 protocols
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Tls11
+
+$WebResponse = Invoke-WebRequest -Uri $url
+# get the list of links, skip the first one ("../") and download the files
+$WebResponse.Links | Select-Object -ExpandProperty href -Skip 1 | ForEach-Object {
+    Write-Host "Downloading file '$_'"
+    $filePath = Join-Path -Path $outputdir -ChildPath $_
+    $fileName = $filePath.split('\')[-1] 
+    $dirPath = $filePath -replace $fileName
+    New-Item -ItemType "directory" -Path $dirPath
+    $fileUrl  = 'https://www.geoffchappell.com/studies/windows/shell/{1}' -f $url.TrimEnd('/'), $_
+    Write-Output $fileUrl
+    Invoke-WebRequest -Uri $fileUrl -OutFile $filePath
+    Start-Sleep -Seconds 1
+}
